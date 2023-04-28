53f2d66a3e2a7006b7c68cb27cf6c905346ef504
MeanNapat012 <napat.kemachaiwead@mail.kmutt.ac.th>
2023-04-28 22:11:03 +0700

    .

---
+++ No.3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+
+        List<int> nodes = new List<int>();
+        while(true)
+        {
+            int node = int.Parse(Console.ReadLine());
+            if(node >= x || node < 0)
+            {
+                break;
+            }
+            nodes.Add(node);
+        }
+
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+
+        int[,] adjMatrix = new int[x, x];
+        for (int i = 0; i < nodes.Count; i += 2)
+        {
+            int fromNode = nodes[i];
+            int toNode = nodes[i + 1];
+            adjMatrix[fromNode, toNode] = 1;
+        }
+
+        int[,] pathMatrix = new int[x, x];
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                if (adjMatrix[i, j] == 1)
+                {
+                    pathMatrix[i, j] = 1;
+                }
+            }
+        }
+
+        for (int k = 0; k < x; k++)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (pathMatrix[i, j] != 1 && pathMatrix[i, k] == 1 && pathMatrix[k, j] == 1)
+                    {
+                        pathMatrix[i, j] = 1;
+                    }
+                }
+            }
+        }
+        if (pathMatrix[start, end] == 1)
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+    }
+}
